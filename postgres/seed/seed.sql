7b9103cdee774f41d6c911f45242652c9217fc69
tyleroakleaf <4921516+toakleaf@users.noreply.github.com>
2019-01-04 19:14:11 -0500

    removed old postgres database configuration files/folder

--- postgres/seed/seed.sql
+++
@@ -1,5 +0,0 @@
--- Seed data with a fake user for testing
-
-insert into users (name, email, joined) values ('a', 'a@a.com', '2018-01-01');
-insert into login (hash, email, isAdmin) values ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'a@a.com', TRUE);
-

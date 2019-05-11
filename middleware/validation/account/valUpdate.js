5ec97a8d98041d3a02226baf5ffdab3da0e05a9b
tyleroakleaf <4921516+toakleaf@users.noreply.github.com>
2019-05-11 15:06:12 -0400

    removed console log

--- middleware/validation/account/valUpdate.js
+++ middleware/validation/account/valUpdate.js
@@ -3,6 +3,7 @@ const db = require('../../../db/db');
 const { MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH } = require('../../../config');
 
 module.exports = (req, res, next) => {
+  console.log(req.body);
   if (
     !req.body.emails &&
     !req.body.password &&
